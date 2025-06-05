371a4fa862272dc59f9f3fd80a539a54ebc90e07
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-06-04 21:45:37 -0300

    finalizar o programa

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿﻿string Pergunta;
+
+Console.Clear();
+
+
+Console.WriteLine("--- Conversa com o Groot ---");
+
+while (true)
+
+{
+    Console.Write("Pergunta: ");
+    Pergunta = Console.ReadLine().ToLower();
+
+
+    if (Pergunta == "tchau") 
+    {
+        Console.WriteLine("         Resposta: Eu sou Groot.");
+
+
+        return;
+
+
+    }
+    else 
+    {
+        Console.WriteLine("         Resposta: Eu sou Groot.");
+        
+    }
+
+};
